b0bd9a36380a2a959ff7e9bf491f71d440e3601b
VeDQ <vedq@fsoft.com.vn>
2020-02-08 22:51:05 +0700

    customer only auto created when it is not existed in db change request body of POST INVOICE

--- src/store-management/Features/Invoices/Create.cs
+++ src/store-management/Features/Invoices/Create.cs
@@ -12,110 +12,123 @@ using System.Threading.Tasks;
 
 namespace store_management.Features.Invoices
 {
-    public class Create
-    {
-        public class InvoiceData
-        {
-            public string ProductId { get; set; }
-            public int ExportAmount { get; set; }
-            public decimal? ExportPrice { get; set; }
+	public class Create
+	{
+		public class InvoiceData
+		{
+			public string ProductId { get; set; }
+			public int ExportAmount { get; set; }
+			public decimal? ExportPrice { get; set; }
+		}
 
-        }
+		public class CustomerData
+		{
+			public string FullName { get; set; }
+			public string Address { get; set; }
+			public string BankAccountNumber { get; set; }
+			public string TaxCode { get; set; }
+		}
 
-        public class InvoiceDataValidator : AbstractValidator<InvoiceData>
-        {
-            public InvoiceDataValidator()
-            {
-                RuleFor(x => x.ProductId).NotNull().NotEmpty();
-            }
-        }
+		public class InvoiceDataValidator : AbstractValidator<InvoiceData>
+		{
+			public InvoiceDataValidator()
+			{
+				RuleFor(x => x.ProductId).NotNull().NotEmpty();
+			}
+		}
 
-        public class Command : IRequest<InvoiceEnvelope>
-        {
-            public List<InvoiceData> InvoiceLinesData { get; set; }
-            public string CustomerTaxCode { get; set; }
-            public string CustomerBankAccountNo { get; set; }
-            public string CustomerName { get; set; }
-            public string CustomerAddress { get; set; }
-        }
+		public class Command : IRequest<InvoiceEnvelope>
+		{
+			public List<InvoiceData> InvoiceLinesData { get; set; }
+			public CustomerData customerData { get; set; }
+		}
 
-        public class CommandValidator : AbstractValidator<Command>
-        {
-            public CommandValidator()
-            {
-                RuleForEach(x => x.InvoiceLinesData).NotNull().NotEmpty()
-                    .SetValidator(new InvoiceDataValidator());
-            }
-        }
+		public class CommandValidator : AbstractValidator<Command>
+		{
+			public CommandValidator()
+			{
+				RuleForEach(x => x.InvoiceLinesData).NotNull().NotEmpty()
+					.SetValidator(new InvoiceDataValidator());
+			}
+		}
 
-        public class Handler : IRequestHandler<Command, InvoiceEnvelope>
-        {
-            private readonly StoreContext _context;
-            
-            public Handler(StoreContext context)
-            {
-                _context = context;
-            }
+		public class Handler : IRequestHandler<Command, InvoiceEnvelope>
+		{
+			private readonly StoreContext _context;
 
-            public async Task<InvoiceEnvelope> Handle(Command request, CancellationToken cancellationToken)
-            {
-                var lines = new List<InvoiceLine>();
+			public Handler(StoreContext context)
+			{
+				_context = context;
+			}
 
-                var customer = new Customer()
-                {
-                    Address = request.CustomerAddress,
-                    BankAccountNumber = request.CustomerBankAccountNo,
-                    FullName = request.CustomerName,
-                    Id = Guid.NewGuid().ToString(),
-                    TaxCode = request.CustomerTaxCode
-                };
+			public async Task<InvoiceEnvelope> Handle(Command request, CancellationToken cancellationToken)
+			{
+				var lines = new List<InvoiceLine>();
 
+				var existedCustomer = await this.HandleExistedCustomer(request.customerData.TaxCode);
+
+				var customer = existedCustomer ?? new Customer()
+				{
+					Address = request.customerData.Address,
+					BankAccountNumber = request.customerData.BankAccountNumber,
+					FullName = request.customerData.FullName,
+					Id = Guid.NewGuid().ToString(),
+					TaxCode = request.customerData.TaxCode
+				};
 
 
-                var invoice = new Invoice()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    ExportDate = DateTime.Now,
-                    InvoiceNo = (new Random()).Next(10000000, 99999999),
-                    CustomerId = customer.Id
-                };
-                
 
-                foreach (var item in request.InvoiceLinesData)
-                {
+				var invoice = new Invoice()
+				{
+					Id = Guid.NewGuid().ToString(),
+					ExportDate = DateTime.Now,
+					InvoiceNo = (new Random()).Next(10000000, 99999999),
+					CustomerId = customer.Id
+				};
 
-                    var notBillingProduct = await _context.ProductExport
-                        .Include(pe => pe.Product)
-                        .FirstOrDefaultAsync(pe => pe.ProductId.Equals(item.ProductId));
 
-                    if (notBillingProduct == null) 
-                        throw new RestException(HttpStatusCode.BadRequest, new { });
-                    if (notBillingProduct.NoBillRemainQuantity < item.ExportAmount)
-                        throw new RestException(HttpStatusCode.BadRequest, new { });
+				foreach (var item in request.InvoiceLinesData)
+				{
 
-                    var exportPrice = item.ExportPrice ?? notBillingProduct.Product.RefPrice;
-                    var invoiceLine = new InvoiceLine()
-                    {
-                        ExportPrice = exportPrice,
-                        Id = Guid.NewGuid().ToString(),
-                        InvoiceId = invoice.Id,
-                        ExportAmount = item.ExportAmount,
-                        Total = exportPrice * item.ExportAmount,
-                        ProductId = item.ProductId
-                    };
-                    lines.Add(invoiceLine);
-                    
-                }
+					var notBillingProduct = await _context.ProductExport
+						.Include(pe => pe.Product)
+						.FirstOrDefaultAsync(pe => pe.ProductId.Equals(item.ProductId));
 
-                invoice.Total = lines.Sum(l => l.Total);
-                await _context.Customer.AddAsync(customer);
-                await _context.Invoice.AddAsync(invoice);
-                await _context.InvoiceLine.AddRangeAsync(lines);
+					if (notBillingProduct == null)
+						throw new RestException(HttpStatusCode.BadRequest, new { });
+					if (notBillingProduct.NoBillRemainQuantity < item.ExportAmount)
+						throw new RestException(HttpStatusCode.BadRequest, new { });
 
-                await _context.SaveChangesAsync();
+					var exportPrice = item.ExportPrice ?? notBillingProduct.Product.RefPrice;
+					var invoiceLine = new InvoiceLine()
+					{
+						ExportPrice = exportPrice,
+						Id = Guid.NewGuid().ToString(),
+						InvoiceId = invoice.Id,
+						ExportAmount = item.ExportAmount,
+						Total = exportPrice * item.ExportAmount,
+						ProductId = item.ProductId
+					};
+					lines.Add(invoiceLine);
 
-                return new InvoiceEnvelope(invoice);
-            }
-        }
-    }
+				}
+
+				invoice.Total = lines.Sum(l => l.Total);
+				await _context.Customer.AddAsync(customer);
+				await _context.Invoice.AddAsync(invoice);
+				await _context.InvoiceLine.AddRangeAsync(lines);
+
+				await _context.SaveChangesAsync();
+
+				return new InvoiceEnvelope(invoice);
+			}
+
+			private async Task<Customer> HandleExistedCustomer(String taxCode)
+			{
+				var customer = await _context.Customer.AsNoTracking()
+				   .FirstOrDefaultAsync(c => c.BankAccountNumber.ToLower().Equals(taxCode));
+				return customer;
+			}
+		}
+	}
 }
